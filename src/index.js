3235477bf1191cfb096fd2f25639393257fa5625
Ismoil Ilxomov <ismoil6ek@gmail.com>
2022-02-02 16:58:18 +0500

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+'use strict';
 
-// You should implement your task here.
+/**
+ * @param {Array<Array<number>>} [matrix]
+ * @returns {Array<number>}
+ */
+function towelSort(matrix = []) {
+    return matrix
+        .map((row, index) => {
+            if (index % 2 === 0) {
+                return row;
+            }
 
-module.exports = function towelSort (matrix) {
-  return [];
+            return row.reverse();
+        })
+        .flat();
 }
+
+module.exports = towelSort;
